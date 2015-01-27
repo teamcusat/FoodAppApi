dd2ea257e6c1028e9143e4e484d849ecbc43aa22
teamcusat <teamcusat.thecompany@gmail.com>
2015-01-27 11:37:47 +0530

    Core

---
+++ FoodApp/FoodApp.Core/Helper.cs
@@ -0,0 +1,21 @@
+﻿
+namespace FoodApp.Core
+{
+    /// <summary>
+    /// The geographic helper
+    /// </summary>
+    public static class GeographicHelper
+    {
+        /// <summary>
+        /// To convert the meters to degree 
+        /// </summary>
+        /// <param name="meter"></param>
+        /// <returns></returns>
+        public static double ConvertToDegrees(double? meter)
+        {
+            var mtr = meter ?? 0;
+            var fact = 11000; //Approximate value
+            return mtr / fact;
+        }
+    }
+}
